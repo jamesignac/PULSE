a904614ec60c006e7c4ca32ad00a2df655a053ee
James Ignacio <143226598+jamesignac@users.noreply.github.com>
2025-08-14 16:29:58 -0400

    Add files via upload

---
+++ specialty.py
@@ -0,0 +1,57 @@
+# Define profession to specialties mapping
+PROFESSION_SPECIALTIES = {
+    "Doctor": [
+        "General Practitioner (GP)",
+        "Surgeon",
+        "Cardiologist",
+        "Neurologist",
+        "Pediatrician",
+        "Psychiatrist",
+        "Dermatologist",
+        "Oncologist",
+        "Radiologist",
+        "Anesthesiologist",
+        "Emergency Medicine Physician",
+        "Other"
+    ],
+    "Nurse": [
+        "Registered Nurse (RN)",
+        "Nurse Practitioner (NP)",
+        "Critical Care Nurse",
+        "Pediatric Nurse",
+        "Oncology Nurse",
+        "Emergency Room (ER) Nurse",
+        "Surgical Nurse",
+        "Other"
+    ],
+    "Pharmacist": [
+        "Clinical Pharmacist",
+        "Hospital Pharmacist",
+        "Oncology Pharmacist",
+        "Compounding Pharmacist",
+        "Other"
+    ],
+    "Physical Therapist": [
+        "Orthopedic Physical Therapist",
+        "Neurological Physical Therapist",
+        "Pediatric Physical Therapist",
+        "Geriatric Physical Therapist",
+        "Sports Physical Therapist",
+        "Other"
+    ],
+    "Researcher": [
+        "Clinical Researcher",
+        "Biomedical Researcher",
+        "Pharmaceutical Researcher",
+        "Epidemiology Researcher",
+        "Other"
+    ],
+    "Administrator": [
+        "Hospital Administrator",
+        "Clinic Manager",
+        "Healthcare Executive",
+        "Practice Manager",
+        "Other"
+    ],
+    "Other": ["Please specify your specialty"]
+}
